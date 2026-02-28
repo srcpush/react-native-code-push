413e7971d42da44b819323c62ca4949cbb4163c0
Hebert <hebertcisco@outlook.com>
2026-02-28 15:51:12 -0300

    feat: update CodePush package instance initialization for debug flag handling

--- react-native.config.js
+++ react-native.config.js
@@ -2,8 +2,10 @@ module.exports = {
     dependency: {
         platforms: {
             android: {
+                packageImportPath:
+                    "import com.microsoft.codepush.react.CodePush;",
                 packageInstance:
-                    "CodePush.getInstance(getResources().getString(R.string.CodePushDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)",
+                    "CodePush.getInstance(getResources().getString(R.string.CodePushDeploymentKey), getApplicationContext(), (0 != (getApplicationContext().getApplicationInfo().flags & android.content.pm.ApplicationInfo.FLAG_DEBUGGABLE)))",
                 sourceDir: './android/app',
             }
         }
