0b4436b5afa65e43625f1d9799302c11977d259b
hebertcisco <hebertcisco@outlook.com>
2026-02-28 22:25:38 -0300

    feat: enhance new architecture support by allowing environment variable for newArchEnabled

--- react-native.config.js
+++ react-native.config.js
@@ -2,10 +2,6 @@ module.exports = {
     dependency: {
         platforms: {
             android: {
-                packageImportPath:
-                    "import com.microsoft.codepush.react.CodePush;",
-                packageInstance:
-                    "CodePush.getInstance(getResources().getString(R.string.CodePushDeploymentKey), getApplicationContext(), (0 != (getApplicationContext().getApplicationInfo().flags & android.content.pm.ApplicationInfo.FLAG_DEBUGGABLE)))",
                 sourceDir: './android/app',
             }
         }
