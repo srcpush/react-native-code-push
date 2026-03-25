2c66d000dcb60b9a18f1a8d905d8ee84adef7068
hebertcisco <hebertcisco@outlook.com>
2026-03-25 19:04:28 -0300

    feat: add package import and instance initialization for CodePush in react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -3,6 +3,8 @@ module.exports = {
         platforms: {
             android: {
                 sourceDir: './android/app',
+                packageImportPath: 'import com.microsoft.codepush.react.CodePush;',
+                packageInstance: 'new CodePush(getResources().getString(R.string.CodePushDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)',
             }
         }
     }
